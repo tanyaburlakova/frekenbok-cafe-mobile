003a8db5effb602de14b2585c8ed73bcc65a7c3c
tanyaburlakova <ta.burlakova@gmail.com>
2016-08-01 18:09:20 +0300

    Map's custom controls

--- app/js/app.js
+++ app/js/app.js
@@ -14,18 +14,19 @@ $(() => {
 
         //Autoplay
         autoplay: true,
-        autoplayTimeout: 8000;
+        autoplayTimeout: 8000,
 
         //Pagination
         dots: true,
 
         // Other
         addClassActive: true,
+        loop: true,
         singleItem: true,
         animateIn: 'fadeIn',
-        loop: true,
-        animateOut: 'fadeOut',
+        animateOut: 'fadeOut'
     });
+
     $('.jsConceptCarousel').owlCarousel({
         items: 1,
 
@@ -34,13 +35,13 @@ $(() => {
 
         // Other
         addClassActive: true,
+        loop: true,
         dots: false,
         nav: true,
         navText: false,
         singleItem: true,
         animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-        loop: true,
+        animateOut: 'fadeOut'
     });
 
     $('.jsReviewsCarousel').owlCarousel({
@@ -51,17 +52,17 @@ $(() => {
 
         // Other
         addClassActive: true,
+        loop: true,
         dots: false,
         nav: true,
         navText: false,
         singleItem: true,
         animateIn: 'fadeIn',
-        animateOut: 'fadeOut',
-        loop: true
+        animateOut: 'fadeOut'
     });
 
     // show/hide site popups
-    $('.jsPopupHandler').on('click', function( e ) {
+    $('.jsPopupHandler').on('click', function(e) {
         e.preventDefault();
 
         var
@@ -69,50 +70,72 @@ $(() => {
             currentPopup = $popups.filter('[data-popup-el="' + currentClass + '"]');
         // vars
 
-        if ( !(currentPopup.hasClass( showPopup ) ) ) {
+        if (!(currentPopup.hasClass(showPopup))) {
             currentPopup.addClass(showPopup);
         } else {
             currentPopup.removeClass(showPopup);
         }
     });
 
-    $('.jsPopupCloser').on('click', function( e ) {
+    $('.jsPopupCloser').on('click', function(e) {
         e.preventDefault();
 
         $(this).closest('.jsPopupEl').removeClass(showPopup);
     });
 
-    // When the window has finished loading create our google map below
-    google.maps.event.addDomListener(window, 'load', init);
+    /**
+     * The HomeControl adds +/- button for the map
+     *
+     */
 
-    function init() {
-        // Basic options for a simple Google Map
-        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+    var map;
+
+    function HomeControl(controlDiv, map) {
+        google.maps.event.addDomListener(zoomout, 'click', function() {
+            var currentZoomLevel = map.getZoom();
+            if (currentZoomLevel != 0) {
+                map.setZoom(currentZoomLevel - 1);
+            }
+
+        });
+
+        google.maps.event.addDomListener(zoomin, 'click', function() {
+            var currentZoomLevel = map.getZoom();
+            if (currentZoomLevel != 21) {
+                map.setZoom(currentZoomLevel + 1);
+            }
+
+        });
+    }
+
+    function initialize() {
+        var mapDiv = document.getElementById('map');
+
         var mapOptions = {
-            // How zoomed in you want the map to start at (always required)
             zoom: 3,
-            disableDefaultUI:true,
+            disableDefaultUI: true,
 
             // The latitude and longitude to center the map (always required)
             center: new google.maps.LatLng(40.6700, -73.9400), // New York
 
             // How you would like to style the map.
             // This is where you would paste any style found on Snazzy Maps.
-            styles: [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#84afa3"},{"lightness":52}]},{"stylers":[{"saturation":-17},{"gamma":0.36}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]}]
-        };
+            styles: [{ "featureType": "administrative", "stylers": [{ "visibility": "off" }] }, { "featureType": "poi", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road", "elementType": "labels", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "water", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "transit", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "landscape", "stylers": [{ "visibility": "simplified" }] }, { "featureType": "road.highway", "stylers": [{ "visibility": "off" }] }, { "featureType": "road.local", "stylers": [{ "visibility": "on" }] }, { "featureType": "road.highway", "elementType": "geometry", "stylers": [{ "visibility": "on" }] }, { "featureType": "water", "stylers": [{ "color": "#84afa3" }, { "lightness": 52 }] }, { "stylers": [{ "saturation": -17 }, { "gamma": 0.36 }] }, { "featureType": "transit.line", "elementType": "geometry", "stylers": [{ "color": "#3f518c" }] }]
+        }
 
-        // Get the HTML DOM element that will contain your map
-        // We are using a div with id="map" seen below in the <body>
-        var mapElement = document.getElementById('map');
 
-        // Create the Google Map using our element and options defined above
-        var map = new google.maps.Map(mapElement, mapOptions);
+        map = new google.maps.Map(mapDiv, mapOptions);
 
-        // Let's also add a marker while we're at it
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(40.6700, -73.9400),
-            map: map,
-            title: 'Snazzy!'
-        });
+        // Create the DIV to hold the control and
+        // call the HomeControl() constructor passing
+        // in this DIV.
+        var homeControlDiv = document.createElement('div');
+        var homeControl = new HomeControl(homeControlDiv, map);
+
+        homeControlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(homeControlDiv);
+
     }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
 });
